4b525f66bf62e12ceea82bf9bfe3f0af8dd645bd
Alex Kavourias <kavourias.a@husky.neu.edu>
2015-10-12 21:58:22 -0400

    Fixed more styling issues, as well as connection errors

---
+++ client.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  var username;
+  var socket;
+  var connected = false;
+  var lastGrey = false; //Used to alternate color of messages;
+  function connect(text) {
+      console.log(text);
+      text = text.split(" ");
+      if (text[0] != "connect" || text.length < 2) {
+	 addMessage("ERROR", "To connect, type 'connect' followed by a username");
+	 return;
+      }
+      username = text.slice(1).join(' ');
+      console.log(text + "\t" + username);
+      socket = io.connect({query: "username=" + username});
+      connected = true;
+      socket.on("newMessage", function(data) {
+	addMessage(data.username, data.message);
+      });
+    }
+    $('button[name=sendMessage]').click(function(e) {
+	  if (!connected) {
+	    console.log("connecting");
+	    connect($("input[name=messageText]").val());
+	    console.log("text: " + $("input[name=messageText]").val());
+	    console.log("done connecting");
+	    socket.emit('newMessage', {'username': "Server", 'message': "User " + username +
+		" has connected."});
+          } else {
+	      socket.emit('newMessage', 
+	          {'username': username, 'message': $('input[name=messageText]').val()});  
+	  }
+	  return false;
+    });
+    function addMessage(username, message) {
+	var li = $("<li></li>",
+		{"class": "message",
+		 "style": "background-color:" + (lastGrey ? "white" : "#eee") + ";list-style-type:none;",
+		 "text": username + ": " + message});
+	$("#list").append(li);
+	lastGrey = !lastGrey;
+	$('input[name=messageText]').val("");
+    }
+});
