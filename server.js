d17ce7eb37d7a3e49c7b8d2aee20647df32d7c62
Alex Kavourias <kavourias.a@husky.neu.edu>
2015-10-12 20:01:32 -0400

    Working chat. Fixed issues with message formatting and connection

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ var server = http.createServer(function(req, resp) {
       var read = fs.createReadStream(__dirname + req.url);
       read.pipe(resp);
       resp.writeHead(200, {"Content-Type": "text/html"});
+    } else if (req.url == "/style.css" || req.url == "/client.js") {
+      resp.writeHead(200, {"Content-Type":  
+		req.url == '/style.css' ? 'text/css' : 'application/javascript'}); 
+      fs.createReadStream(__dirname + req.url).pipe(resp);
     }    
 });
 
 io.sockets.on("connection", function(socket) {
     console.log("connected: " + socket.handshake.query.username);
+    console.log(socket.handshake.query.username);
     currentUsers.push(socket.handshake.query.username);
     socket.on('newMessage', function(msg) {
         io.emit('newMessage', msg);
